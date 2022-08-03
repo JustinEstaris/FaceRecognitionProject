132f8fa4683dec7d3d47edc5ac4d664ce6918453
Justin Estaris <JustinEstaris@users.noreply.github.com>
2022-08-03 17:14:48 -0400

    Added particle system

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -3,20 +3,33 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import particlesOptions from "./particles.json";
+
 import './App.css';
-
-
 
 class App extends Component {
   render() {
+    const particlesInit = async (main) => {
+      await loadFull(main);
+    };
+
+    const particlesLoaded = (container) => {
+      console.log(container);
+    };
+
     return (
-      <div className="App">
+      <div className="App" >
+
         <Navigation />
         <Logo />
         <Rank />
         <ImageLinkForm />
-
+        <Particles className="particles" options={particlesOptions} init={particlesInit} loaded={particlesLoaded} />
         {/*<FaceRecognition /> */}
+
       </div>
     );
   }
